4b43d175ead41d2e269e94c2257071028e0caa91
marcusdmc <marcusdmc@gmail.com>
2019-08-07 19:13:00 -0400

    Thank you!

---
+++ src/home/FizzBuzz.java
@@ -0,0 +1,11 @@
+package home;
+
+public class FizzBuzz {
+    private String fizzBuzz;
+
+    public FizzBuzz(String fizzBuzz){
+        this.fizzBuzz = fizzBuzz;
+    }
+
+    public
+}
---
+++ test/home/FizzBuzzTest.java
@@ -0,0 +1,14 @@
+package home;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    public void doYouFizzOrBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("12",fizzBuzzTest());
+    }
+
+}
