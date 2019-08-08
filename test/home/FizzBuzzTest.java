0df9b3b0631f4642d539b48e18bf3b436b6e9dc6
marcusdmc <marcusdmc@gmail.com>
2019-08-08 08:48:16 -0400

    Thank you!

--- test/home/FizzBuzzTest.java
+++ test/home/FizzBuzzTest.java
@@ -6,9 +6,30 @@ import static junit.framework.TestCase.assertEquals;
 
 public class FizzBuzzTest {
     @Test
-    public void doYouFizzOrBuzz(){
-        FizzBuzz fizzBuzz = new FizzBuzz(15);
+    public void doYouFizz(){
+        FizzBuzz fizzBuzz = new FizzBuzz(12);
+        assertEquals("fizz",fizzBuzz.calculateFizzOrBuzz());
+    }
+
+    @Test
+    public void doYouBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz(20);
+        assertEquals("buzz",fizzBuzz.calculateFizzOrBuzz());
+    }
+
+    @Test
+    public void doYouFizzBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz(30);
         assertEquals("fizzbuzz",fizzBuzz.calculateFizzOrBuzz());
     }
+
+    @Test
+    public void doYouNotFizzOrBuzz(){
+        FizzBuzz fizzBuzz = new FizzBuzz(17);
+        assertEquals(String.valueOf(17),fizzBuzz.calculateFizzOrBuzz());
+    }
+
+
+
 
 }
