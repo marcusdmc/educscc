98b237caf8842603496feeb7198f289f87b932e8
marcusdmc <marcusdmc@gmail.com>
2019-08-07 21:56:54 -0400

    Thank you!

--- src/home/FizzBuzz.java
+++ src/home/FizzBuzz.java
@@ -1,14 +1,37 @@
 package home;
 
 public class FizzBuzz {
-    private String fizzBuzz;
+    private int fizzBuzz;
 
-    public FizzBuzz(String fizzBuzz){
+    public FizzBuzz(int fizzBuzz){
         this.fizzBuzz = fizzBuzz;
     }
 
     public String calculateFizzOrBuzz(){
-        return fizzBuzz;
+        if (fizzBuzz == 0){
+            return noFizzOrBuzz();
+        }
+        else if(fizzBuzz%3 + fizzBuzz%5 == 0){
+            return typeFizz()+typeBuzz();
+        }
+        else if(fizzBuzz%3 == 0){
+            return typeFizz();
+        }
+        else if(fizzBuzz%5 == 0){
+            return typeBuzz();
+        }
+        else return noFizzOrBuzz();
+    }
 
+    private String typeFizz(){
+        return "fizz";
+    }
+
+    private String typeBuzz(){
+        return "buzz";
+    }
+
+    private String noFizzOrBuzz(){
+        return String.valueOf(fizzBuzz);
     }
 }
